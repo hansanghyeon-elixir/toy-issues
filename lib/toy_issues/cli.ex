8be5babdd06613381d6e1ea93756fe3e186d6344
Hansanghyeon <999@hyeon.pro>
2024-11-14 03:56:38 +0000

    feat: cli.exs 테스트 코드 추가

--- lib/toy_issues/cli.ex
+++ lib/toy_issues/cli.ex
@@ -16,10 +16,11 @@ defmodule ToyIssues.Cli do
   """
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+
     case parse do
-      { [help: true], _, _ } -> :help
-      { _, [user, project, count], _ } -> { user, project, count}
-      { _, [user, project], _ } -> { user, project, @default_count }
+      {[help: true], _, _} -> :help
+      {_, [user, project, count], _} -> {user, project, count |> String.to_integer()}
+      {_, [user, project], _} -> {user, project, @default_count}
       _ -> :help
     end
   end
