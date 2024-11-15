2718f6a594b284b4d0b40e7e49f5bd969b7fa983
Hansanghyeon <999@hyeon.pro>
2024-11-15 10:40:49 +0000

    refactor: global config 사용

---
+++ config/config.exs
@@ -0,0 +1,2 @@
+import Config
+config :toy_issues, github_url: "https://api.github.com"
--- lib/toy_issues/github_issues.ex
+++ lib/toy_issues/github_issues.ex
@@ -6,8 +6,10 @@ defmodule ToyIssues.GithubIssues do
     |> handle_response
   end
 
+  # 컴파일 시점에 값을 가져오기 위해 모듈 속성을 사용한다.
+  @github_url Application.get_env(:toy_issues, :github_url)
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
