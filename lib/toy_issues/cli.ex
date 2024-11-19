e8edf0789c241bda1cba25ea049e71ff2c3f7257
Hansanghyeon <999@hyeon.pro>
2024-11-19 11:41:05 +0000

    feat: main 함수 등록

--- lib/toy_issues/cli.ex
+++ lib/toy_issues/cli.ex
@@ -6,7 +6,7 @@ defmodule ToyIssues.CLI do
   명령줄 파싱을 수행한 뒤, 각종 함수를 호출해
   깃허브 프로젝트의 최근 _n_개 이슈를 표 형식으로 만들어 출력한다.
   """
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
@@ -50,6 +50,7 @@ defmodule ToyIssues.CLI do
     |> decode_response
     |> sort_into_descending_order
     |> last(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
